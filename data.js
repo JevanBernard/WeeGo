7505c2ac213b80ed847aeb4233d20995d4c2a744
JevanBernard <148156356+JevanBernard@users.noreply.github.com>
2025-04-17 09:06:58 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,85 @@
+const allItems = [
+    // data dummy 
+    { 
+        id: 'alaskaki1',
+        type: 'alas kaki',
+        name: 'Sandal Platform Putih',
+        img: 'weego/Alas Kaki 1.jpg',
+        color: 'putih',
+        style: 'formal',
+        affiliateLink: 'https://tokopedia.com/sandal-platform-putih' 
+    },
+
+    {
+        id: 2,
+        type: 'atasan',
+        name: 'Kaos Hitam',
+        img: 'assets/atasan2.jpg',
+        color: 'hitam',
+        style: 'kasual',
+        affiliateLink: '#' 
+    },
+
+    {
+        id: 3,
+        type: 'bawahan',
+        name: 'Celana Jeans',
+        img: 'assets/bawahan1.jpg',
+        color: 'biru',
+        style: 'kasual',
+        affiliateLink: '#' 
+    },
+
+    {
+        id: 4,
+        type: 'dress',
+        name: 'Dress Polos',
+        img: 'assets/dress1.jpg',
+        color: 'nude',
+        style: 'formal',
+        affiliateLink: '#' 
+    },
+
+    {
+        id: 5,
+        type: 'sepatu',
+        name: 'Sneakers Putih',
+        img: 'assets/sepatu1.jpg',
+        color: 'putih',
+        style: 'streetwear',
+        affiliateLink: '#' 
+    },
+
+    {
+        id: 6,
+        type: 'tas',
+        name: 'Tote Bag',
+        img: 'assets/tas1.jpg',
+        color: 'hitam',
+        style: 'kasual',
+        affiliateLink: '#' 
+    },
+
+    {
+        id: 7,
+        type: 'gelang',
+        name: 'Gelang Emas',
+        img: 'assets/gelang1.jpg',
+        color: 'nude',
+        style: 'formal',
+        affiliateLink: '#' 
+    },
+
+    {
+        id: 8, 
+        type: 'jam', 
+        name: 'Jam Hitam', 
+        img: 'assets/jam1.jpg', 
+        color: 'hitam', 
+        style: 'kasual', 
+        affiliateLink: '#' 
+    }
+  
+    
+  ];
+  
